ffe53db411f23e6f4fe22fede20cf72d5976eac5
pedromchd <pedrogmachado28@outlook.com>
2023-12-03 22:59:39 -0300

    fetch forecast

---
+++ server/controllers/WeatherController.js
@@ -0,0 +1,15 @@
+const Weather = require('../models/Weather');
+
+async function fetchForecast({ lat, long }) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,weather_code,wind_speed_10m&hourly=temperature_2m,precipitation_probability,weather_code,uv_index,is_day&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max&timezone=America%2FSao_Paulo&past_days=1`;
+    const response = await fetch(url);
+    const forecast = await response.json();
+
+    return forecast;
+}
+
+exports.fetchForecast = async (req, res) => {
+    const forecast = await fetchForecast(req.params);
+
+    res.json(forecast);
+};
--- server/routers/urls.js
+++ server/routers/urls.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const WeatherController = require('../controllers/WeatherController');
+
 const router = express.Router();
+
+router.get('/api/weather/:lat/:long', WeatherController.fetchForecast);
 
 router.get('/api', (req, res) => {
     res.json({ message: 'Hello from server!' });
